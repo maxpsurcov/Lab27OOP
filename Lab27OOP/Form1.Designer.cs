ad3097e3adc6d40d58bd0bfe16f04b79493f37ca
maxpsurcov <max.psurcov1@gmail.com>
2023-05-06 13:12:13 +0300

    Добавьте файлы проекта.

---
+++ Lab27OOP/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Lab27OOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxFilterDirectories = new System.Windows.Forms.CheckBox();
+            this.checkBoxFilterFiles = new System.Windows.Forms.CheckBox();
+            this.textBoxFileContent = new System.Windows.Forms.TextBox();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.textBoxSecurity = new System.Windows.Forms.TextBox();
+            this.pictureBoxImage = new System.Windows.Forms.PictureBox();
+            this.propertyGrid = new System.Windows.Forms.PropertyGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBoxFilterDirectories
+            // 
+            this.checkBoxFilterDirectories.AutoSize = true;
+            this.checkBoxFilterDirectories.Location = new System.Drawing.Point(176, 126);
+            this.checkBoxFilterDirectories.Name = "checkBoxFilterDirectories";
+            this.checkBoxFilterDirectories.Size = new System.Drawing.Size(258, 29);
+            this.checkBoxFilterDirectories.TabIndex = 0;
+            this.checkBoxFilterDirectories.Text = "checkBoxFilterDirectories";
+            this.checkBoxFilterDirectories.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxFilterFiles
+            // 
+            this.checkBoxFilterFiles.AutoSize = true;
+            this.checkBoxFilterFiles.Location = new System.Drawing.Point(176, 175);
+            this.checkBoxFilterFiles.Name = "checkBoxFilterFiles";
+            this.checkBoxFilterFiles.Size = new System.Drawing.Size(207, 29);
+            this.checkBoxFilterFiles.TabIndex = 1;
+            this.checkBoxFilterFiles.Text = "checkBoxFilterFiles";
+            this.checkBoxFilterFiles.UseVisualStyleBackColor = true;
+            // 
+            // textBoxFileContent
+            // 
+            this.textBoxFileContent.Location = new System.Drawing.Point(176, 235);
+            this.textBoxFileContent.Name = "textBoxFileContent";
+            this.textBoxFileContent.Size = new System.Drawing.Size(258, 29);
+            this.textBoxFileContent.TabIndex = 2;
+            // 
+            // treeView
+            // 
+            this.treeView.Location = new System.Drawing.Point(499, 78);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(252, 88);
+            this.treeView.TabIndex = 3;
+            // 
+            // textBoxSecurity
+            // 
+            this.textBoxSecurity.Location = new System.Drawing.Point(167, 397);
+            this.textBoxSecurity.Name = "textBoxSecurity";
+            this.textBoxSecurity.Size = new System.Drawing.Size(258, 29);
+            this.textBoxSecurity.TabIndex = 4;
+            // 
+            // pictureBoxImage
+            // 
+            this.pictureBoxImage.Location = new System.Drawing.Point(482, 235);
+            this.pictureBoxImage.Name = "pictureBoxImage";
+            this.pictureBoxImage.Size = new System.Drawing.Size(269, 191);
+            this.pictureBoxImage.TabIndex = 5;
+            this.pictureBoxImage.TabStop = false;
+            // 
+            // propertyGrid
+            // 
+            this.propertyGrid.Location = new System.Drawing.Point(12, 36);
+            this.propertyGrid.Name = "propertyGrid";
+            this.propertyGrid.Size = new System.Drawing.Size(130, 130);
+            this.propertyGrid.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 622);
+            this.Controls.Add(this.propertyGrid);
+            this.Controls.Add(this.pictureBoxImage);
+            this.Controls.Add(this.textBoxSecurity);
+            this.Controls.Add(this.treeView);
+            this.Controls.Add(this.textBoxFileContent);
+            this.Controls.Add(this.checkBoxFilterFiles);
+            this.Controls.Add(this.checkBoxFilterDirectories);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxFilterDirectories;
+        private System.Windows.Forms.CheckBox checkBoxFilterFiles;
+        private System.Windows.Forms.TextBox textBoxFileContent;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.TextBox textBoxSecurity;
+        private System.Windows.Forms.PictureBox pictureBoxImage;
+        private System.Windows.Forms.PropertyGrid propertyGrid;
+    }
+}
+
---
+++ Lab27OOP/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab27OOP
+{
+    public partial class Form1 : Form
+    {
+        private string selectedFilePath;
+        private string selectedDirectoryPath;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDrives();
+        }
+        private void LoadDrives()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                TreeNode node = new TreeNode(drive.Name);
+                node.Tag = drive.RootDirectory;
+                node.Nodes.Add("");
+                treeView.Nodes.Add(node);
+            }
+        }
+
+        private void LoadDirectories(TreeNode parentNode)
+        {
+            try
+            {
+                DirectoryInfo directory = (DirectoryInfo)parentNode.Tag;
+                directory.GetDirectories();
+                foreach (DirectoryInfo subdirectory in directory.GetDirectories())
+                {
+                    TreeNode node = new TreeNode(subdirectory.Name);
+                    node.Tag = subdirectory;
+                    node.Nodes.Add("");
+                    parentNode.Nodes.Add(node);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // не маємо дозволу на доступ до цього каталогу
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LoadFiles(TreeNode parentNode)
+        {
+            try
+            {
+                DirectoryInfo directory = (DirectoryInfo)parentNode.Tag;
+                foreach (FileInfo file in directory.GetFiles())
+                {
+                    // фільтрація списку файлів
+                    if (Path.GetExtension(file.Name) == ".txt")
+                    {
+                        TreeNode node = new TreeNode(file.Name);
+                        node.Tag = file;
+                        parentNode.Nodes.Add(node);
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // не маємо дозволу на доступ до цього каталогу
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void treeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            if (e.Node.Nodes.Count == 1 && e.Node.Nodes[0].Text == "")
+            {
+                e.Node.Nodes.Clear();
+                LoadDirectories(e.Node);
+                LoadFiles(e.Node);
+            }
+        }
+
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Tag is DriveInfo)
+            {
+                // перегляд основних властивостей виділеного диску
+                DriveInfo drive = (DriveInfo)e.Node.Tag;
+                propertyGrid.SelectedObject = drive;
+            }
+            else if (e.Node.Tag is DirectoryInfo)
+            {
+                // перегляд основних властивостей виділеного каталогу
+                DirectoryInfo directory = (DirectoryInfo)e.Node.Tag;
+                propertyGrid.SelectedObject = directory;
+
+                // фільтрація списку каталогів
+                if (checkBoxFilterDirectories.Checked)
+                {
+                    e.Node.Nodes.Clear();
+                    LoadDirectories(e.Node);
+                }
+            }
+            else if (e.Node.Tag is FileInfo)
+            {   // перегляд основних властивостей виділеного файлу
+                FileInfo file = (FileInfo)e.Node.Tag;
+                propertyGrid.SelectedObject = file;
+
+                // фільтрація списку файлів
+                if (checkBoxFilterFiles.Checked)
+                {
+                    e.Node.Parent.Nodes.Clear();
+                    LoadFiles(e.Node.Parent);
+                }
+
+                // перегляд вмісту текстових файлів
+                if (Path.GetExtension(file.Name) == ".txt")
+                {
+                    selectedFilePath = file.FullName;
+                    textBoxFileContent.Text = File.ReadAllText(selectedFilePath);
+                }
+                else
+                {
+                    selectedFilePath = null;
+                    textBoxFileContent.Clear();
+                }
+
+                // перегляд вмісту графічних файлів
+                if (Path.GetExtension(file.Name) == ".jpg" || Path.GetExtension(file.Name) == ".png")
+                {
+                    pictureBoxImage.Image = Image.FromFile(file.FullName);
+                }
+                else
+                {
+                    pictureBoxImage.Image = null;
+                }
+
+                // перегляд атрибутів безпеки файлів та каталогів
+                try
+                {
+                    FileSystemSecurity security = File.GetAccessControl(file.FullName);
+                    textBoxSecurity.Text = security.GetSecurityDescriptorSddlForm(AccessControlSections.All);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    textBoxSecurity.Text = "Не маємо дозволу на доступ до цього файлу";
+                }
+                catch (Exception ex)
+                {
+                    textBoxSecurity.Text = ex.Message;
+                }
+            }
+        }
+        private void checkBoxFilterDirectories_CheckedChanged(object sender, EventArgs e)
+        {
+            if (treeView.SelectedNode != null && treeView.SelectedNode.Tag is DirectoryInfo)
+            {
+                treeView.SelectedNode.Nodes.Clear();
+                LoadDirectories(treeView.SelectedNode);
+            }
+        }
+
+        private void checkBoxFilterFiles_CheckedChanged(object sender, EventArgs e)
+        {
+            if (treeView.SelectedNode != null && treeView.SelectedNode.Tag is DirectoryInfo)
+            {
+                treeView.SelectedNode.Nodes.Clear();
+                LoadFiles(treeView.SelectedNode);
+            }
+        }
+
+        private void textBoxFileContent_TextChanged(object sender, EventArgs e)
+        {
+            if (selectedFilePath != null)
+            {
+                File.WriteAllText(selectedFilePath, textBoxFileContent.Text);
+            }
+        }
+    }
+}
